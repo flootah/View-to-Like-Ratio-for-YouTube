26a405e843bb450302ed1ecf7c19b7dd85b43638
flootah <ed.saenz@protonmail.ch>
2021-11-30 16:52:45 -0800

    v0.1 update
    - creates div and updates values on yt-data-updated event and document.readyState != loading.
    - options for ratio, percentage, and both
    - css now looks in place with yt

---
+++ options.js
@@ -0,0 +1,30 @@
+// Saves options to chrome.storage
+function save_options() {
+    var displaypref = document.querySelector('input[name="radio"]:checked').value;
+    chrome.storage.sync.set({
+      displaypreference: displaypref
+    }, function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+  }
+  
+  // Restores select box and checkbox state using the preferences
+  // stored in chrome.storage.
+  function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+      displaypreference: 'ratio'
+    }, function(items) {
+      document.querySelector('input[value = "' + items.displaypreference + '"').checked = true;
+    });
+  }
+
+
+  document.addEventListener('DOMContentLoaded', restore_options);
+  document.getElementById('save').addEventListener('click',
+      save_options);
